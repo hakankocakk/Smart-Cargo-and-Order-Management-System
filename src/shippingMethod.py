f7c5c4c0003b5a8e7f5fa6736aa8baa046fc0841
leylagur <gulnur.gur@std.yildiz.edu.tr>
2025-05-17 23:45:12 +0300

    Add files via upload

---
+++ src/shippingMethod.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+class ShippingMethod(ABC):
+    @abstractmethod
+    def calculateCoat(self):
+        pass
+
+class FastShippimg(ShippingMethod):
+    def calculateCoat(self):
+        return 20.0
+class CheapShipping(ShippingMethod):
+    def calculateCoat(self):
+        return 5.0
+class DroneShipping(ShippingMethod):
+    def calculateCoat(self):
+        return 50.0
