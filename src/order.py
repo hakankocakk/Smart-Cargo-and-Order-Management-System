437a6bd0c6f55584cef0fc0d18cf6c14f2e1da8e
Nur <mgnurgumus@gmail.com>
2025-05-18 21:03:45 +0300

    order.py and orderstatus.py added
    sonra düzenlicem

---
+++ src/order.py
@@ -0,0 +1,39 @@
+from typing import List
+from customer import Customer
+from product import Product
+from orderstatus import OrderStatus
+from shippingMethod import ShippingMethod
+
+class Order:
+    def __init__(self, order_id: int, customer: Customer,
+                 products: List[Product],
+                 status: OrderStatus,
+                 shipping_method: ShippingMethod):
+        self.__id = order_id
+        self.__customer = customer
+        self.__products = products
+        self.__status = status
+        self.__shipping_method = shipping_method
+
+    def calculate_total(self) -> float:
+        """Calculate total price of products plus shipping cost."""
+        product_total = sum(product.price for product in self.__products)
+        shipping_cost = self.__shipping_method.calculateCoat()
+        return product_total + shipping_cost
+
+    def update_status(self, new_status: OrderStatus):
+        """Update the order status."""
+        self.__status = new_status
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def status(self):
+        return self.__status
+
+    @property
+    def customer(self):
+        return self.__customer
+
